332538c2cd7a49335a333a166aacc0b0d0329990
chloexu310 <choichole@gmail.com>
2020-09-06 01:12:16 +0500

    80% Completed

--- Intermediate SQL Server/Math Functions.sql
+++ Intermediate SQL Server/Math Functions.sql
@@ -37,3 +37,47 @@ GROUP BY ___
 SELECT OrderDate, ShipDate,
     ___(___, OrderDate, ShipDate) AS Duration
 FROM Shipments
+
+
+-- Rounding numbers
+-- Sometimes, you only
+-- care about the whole dollar amount and want to ignore the decimal values of the cost. In this exercise, you will round the cost to the nearest dollar.
+
+-- Round Cost to the nearest dollar
+SELECT Cost,
+    ROUND(Cost, 0) AS RoundedCost
+FROM Shipments
+
+-- Exercise
+-- Truncating
+-- numbers
+-- Since rounding can sometimes be misleading, i.e., $16.8 becomes $17
+-- while $16.4 remains $16, you may want to
+-- truncate the values after the decimal instead of rounding them. When you
+-- truncate the numbers, both $16.8 and $16.4 remain $16. In this exercise, you will do just that,
+-- truncate the Cost column to a whole number.
+
+-- Truncate cost to whole number
+SELECT Cost,
+    ROUND(Cost, 0, 1) AS TruncateCost
+FROM Shipments
+
+-- Calculating the absolute
+-- value
+-- The Shipments table contains some bad data. There was a problem
+-- with the scales, and the weights show up as negative numbers. In this exercise, you will write a query to convert all negative weights to positive weights.
+
+-- Return the absolute value of DeliveryWeight
+SELECT DeliveryWeight,
+    ABS(DeliveryWeight) AS AbsoluteValue
+FROM Shipments
+
+-- Calculating squares
+-- and square roots
+-- It's time for you to practice calculating squares and square roots of columns.
+
+-- Return the square and square root of WeightValue
+SELECT WeightValue,
+    SQUARE(WeightValue) AS WeightSquare,
+    SQRT(WeightValue) AS WeightSqrt
+FROM Shipments
---
+++ Intermediate SQL Server/Window Functions.sql
@@ -0,0 +1,19 @@
+-- -- Window Functions
+-- In the final
+-- chapter of this course, you will work
+-- with partitions of data and window functions to calculate several summary stats and see how easy it is to
+-- create running totals and compute the mode of numeric columns.
+
+-- Window functions
+-- with aggregations
+-- (I)
+-- To familiarize yourself
+-- with the window functions, you will work
+-- with the Orders table in this chapter. Recall that using OVER
+-- (), you can
+-- create a window for the entire table. To
+-- create partitions using a specific column, you need to
+-- use OVER
+-- () along
+-- with PARTITION BY.
+
